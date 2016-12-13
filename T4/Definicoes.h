12c37dce47bb64f33a780b4fe095e095c7b2ddd3
rafarubim <rafael.rubim@bol.com.br>
2016-12-13 01:02:17 -0200

    Adicionados alguns arquivos do T2

---
+++ T4/Definicoes.h
@@ -0,0 +1,14 @@
+#pragma once
+
+typedef enum {
+	SEM_COR = 0,
+    AZUL = 1,
+    VERMELHO = 2,
+    VERDE = 3,
+    AMARELO = 4
+} DEF_tpCor;
+ 
+typedef enum {
+    True,
+    False
+} DEF_tpBool;

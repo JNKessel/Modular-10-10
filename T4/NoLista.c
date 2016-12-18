c15fc1a2ebda4778019a1deea71c2224fd7f1856
phfrayha <pedro.frayha@gmail.com>
2016-12-18 19:15:58 -0200

    Update NoLista.c

--- T4/NoLista.c
+++ T4/NoLista.c
@@ -1,6 +1,32 @@
+/*******************************************************************************************************************************
+*	$MCD Módulo de definição: Módulo No Lista
+*	Arquivo:	NoLista.c
+*	Letras identificadoras:		NOLST
+*
+*	Autores:	rrc
+*			phf
+*			jnk
+*
+*	$HA Histórico de evolução:
+*		Versâo	Autor	Data		Observações
+*		1.00	rrc		17/10/2016	Começo de implementação, maioria das funções ainda não implementadas
+*		1.10	rrc		09/12/2016	Documentação de funções
+*		2.00	rrc		13/12/2016	Implementação de outras funções
+*		3.00	rrc		14/12/2016	Fim das implementações
+*		4.00	rrc		15/12/2016	Adições ao módulo, correção de bugs
+*		5.00	rrc		18/12/2016	Várias adições de funções a serem usadas pelo módulo ITFC
+*******************************************************************************************************************************/
+
 #include "NoLista.h"
 
 #include <stdlib.h>
+
+/***********************************************************************
+*
+*  $TC Tipo de dados: NOLST descritor da estrutura do No de lista duplamente encadeada
+*
+*
+***********************************************************************/
 
 typedef struct NOLST_tgNoLista {
 	NOLST_tppNoLista pAnt;
@@ -8,6 +34,11 @@ typedef struct NOLST_tgNoLista {
 	void* pInfo;
 } NOLST_tpNoLista;
 
+
+/***************************************************************************
+*
+*  Função: NOLST  &Criar Noh
+*  ****/
 
 NOLST_tpCondRet NOLST_CriarNoh(NOLST_tppNoLista* pNohRet, void* pInfo, NOLST_tppNoLista pNohAnterior,NOLST_tppNoLista pNoProximo) {
 	NOLST_tpCondRet debug;
@@ -25,7 +56,13 @@ NOLST_tpCondRet NOLST_CriarNoh(NOLST_tppNoLista* pNohRet, void* pInfo, NOLST_tpp
     }//if
 	(*pNohRet)->pInfo = pInfo;
 	return NOLST_CondRetOK;
-}
+} /* Fim Funcao: NOLST &Criar Noh */
+
+
+/***************************************************************************
+*
+*  Função: NOLST  &Destruir Noh
+*  ****/
 
 NOLST_tpCondRet  NOLST_DestruirNoh(NOLST_tppNoLista pNoLst) {
 	NOLST_tpCondRet debug;
@@ -38,7 +75,13 @@ NOLST_tpCondRet  NOLST_DestruirNoh(NOLST_tppNoLista pNoLst) {
     }//if
 	free(pNoLst); // Implementacao nao assume erro em free
 	return NOLST_CondRetOK;
-}
+} /* Fim Funcao: NOLST &Destruir Noh  */
+
+
+/***************************************************************************
+*
+*  Função: NOLST  &Colocar Noh Em Frente
+*  ****/
 
 NOLST_tpCondRet  NOLST_ColocarNohEmFrente(NOLST_tppNoLista pNoLst, NOLST_tppNoLista pNovoNo) {
 	NOLST_tpCondRet debug;
@@ -60,7 +103,13 @@ NOLST_tpCondRet  NOLST_ColocarNohEmFrente(NOLST_tppNoLista pNoLst, NOLST_tppNoLi
         return debug; // Se Ligar nao retornou OK
     }//if
 	return NOLST_CondRetOK;
-}
+} /* Fim Funcao: NOLST &Colocar Noh Em Frente */
+
+
+/***************************************************************************
+*
+*  Função: NOLST  &Colocar Noh Atras
+*  ****/
 
 NOLST_tpCondRet  NOLST_ColocarNohAtras(NOLST_tppNoLista pNoLst, NOLST_tppNoLista pNovoNo) {
 	NOLST_tpCondRet debug;
@@ -82,7 +131,13 @@ NOLST_tpCondRet  NOLST_ColocarNohAtras(NOLST_tppNoLista pNoLst, NOLST_tppNoLista
         return debug; // Se Ligar nao retornou OK
     }//if
 	return NOLST_CondRetOK;
-}
+} /* Fim funcao: NOLST &Colocar Noh Atras */
+
+
+/***************************************************************************
+*
+*  Função: NOLST  &Obter Prox Noh
+*  ****/
 
 NOLST_tpCondRet  NOLST_ObterProxNoh(NOLST_tppNoLista pNoLst, NOLST_tppNoLista* pNoProxRet) {
     if (pNoLst == NULL){
@@ -90,7 +145,13 @@ NOLST_tpCondRet  NOLST_ObterProxNoh(NOLST_tppNoLista pNoLst, NOLST_tppNoLista* p
     }//if
 	*pNoProxRet = pNoLst->pProx;
 	return NOLST_CondRetOK;
-}
+} /* Fim Funcao: NOLST &Obter Prox Noh */
+
+
+/***************************************************************************
+*
+*  Função: NOLST  &Obter Noh Previo
+*  ****/
 
 NOLST_tpCondRet  NOLST_ObterNohPrevio(NOLST_tppNoLista pNoLst, NOLST_tppNoLista* pNoPrevioRet) {
     if (pNoLst == NULL){
@@ -98,7 +159,13 @@ NOLST_tpCondRet  NOLST_ObterNohPrevio(NOLST_tppNoLista pNoLst, NOLST_tppNoLista*
     }//if
 	*pNoPrevioRet = pNoLst->pAnt;
 	return NOLST_CondRetOK;
-}
+} /* Fim Funcao: NOLST &Obter Noh Previo */
+
+
+/***************************************************************************
+*
+*  Função: NOLST  &Atribuir Info Noh
+*  ****/
 
 NOLST_tpCondRet  NOLST_AtribuirInfoNoh(NOLST_tppNoLista pNoLst, void* pInfo) {
     if (pNoLst == NULL){
@@ -106,7 +173,13 @@ NOLST_tpCondRet  NOLST_AtribuirInfoNoh(NOLST_tppNoLista pNoLst, void* pInfo) {
     }//if
 	pNoLst->pInfo = pInfo;
 	return NOLST_CondRetOK;
-}
+} /* Fim Funcao: NOLST &Atribuir Info Noh */
+
+
+/***************************************************************************
+*
+*  Função: NOLST  &Obter Info Noh
+*  ****/
 
 NOLST_tpCondRet  NOLST_ObterInfoNoh(NOLST_tppNoLista pNoLst, void** pInfoRet) {
     if (pNoLst == NULL){
@@ -114,7 +187,13 @@ NOLST_tpCondRet  NOLST_ObterInfoNoh(NOLST_tppNoLista pNoLst, void** pInfoRet) {
     }//if
 	*pInfoRet = pNoLst->pInfo;
 	return NOLST_CondRetOK;
-}
+}/* Fim funcao: NOLST &Obter Info Noh */
+
+
+/***************************************************************************
+*
+*  Função: NOLST  &Ligar Nos
+*  ****/
 
 NOLST_tpCondRet NOLST_LigarNos(NOLST_tppNoLista pEsq, NOLST_tppNoLista pDir) {
     if (pEsq){
@@ -124,4 +203,7 @@ NOLST_tpCondRet NOLST_LigarNos(NOLST_tppNoLista pEsq, NOLST_tppNoLista pDir) {
 		pDir->pAnt = pEsq;
     }//if
 	return NOLST_CondRetOK;
-}
+} /* Fim funcao: NOLST &Ligar Nos */
+
+/********** Fim do módulo de implementação: NOLST No de lista duplamente encadeada **********/
+
